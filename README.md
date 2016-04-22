bcdf947788f81dc3b3663abcea45df2fff4a9d09
root <root@ip-172-31-8-141.us-west-2.compute.internal>
2016-04-22 05:56:22 +0000

    Init Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # probable-carnival
+
+spark-submit --class Kmeans --master local[2] --jars $(find /root/JCuda-All-0.7.5-bin-Linux-x86_64/| grep '\.jar'|tr '\n' ',')  target/scala-2.10/kmeans_2.10-1.0.jar
---
+++ src/main/scala/cuda_kmeans.scala
@@ -0,0 +1,321 @@
+import jcuda.driver.JCudaDriver._
+import java.io._
+import jcuda._
+import jcuda.driver._
+import jcuda.runtime._
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd._
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql._
+import org.apache.spark.sql.SQLContext._
+//remove if not needed
+import scala.collection.JavaConversions._
+
+
+object Kmeans {
+
+  def main(args: Array[String]) {
+
+    var filename = "/root/kmeans/Image_data/color100.txt"
+    val numObjs = Array(0) 
+    val numCoords = Array(0) 
+    val numClusters = 7
+    val threshold = 0.001f
+    val loop_iterations = Array(0) 
+    
+    val objects = read_file(filename, numObjs, numCoords)
+    assert(objects.length != 0)
+
+    val membership = Array.ofDim[Int](numObjs(0))
+
+    val clusters = cuda_kmeans(objects, numCoords(0), numObjs(0), numClusters, threshold, membership, loop_iterations)
+    filename = "/root/kmeans_spark/out.txt"
+    write_file(filename, numClusters, numObjs(0), numCoords(0), clusters, membership)
+
+  }
+
+  private def preparePtxFile(cuFileName: String): String = {
+    var endIndex = cuFileName.lastIndexOf('.')
+    if (endIndex == -1) {
+      endIndex = cuFileName.length - 1
+    }
+    val ptxFileName = cuFileName.substring(0, endIndex + 1) + "ptx"
+    val ptxFile = new File(ptxFileName)
+    if (ptxFile.exists()) {
+      return ptxFileName
+    }
+    val cuFile = new File(cuFileName)
+    if (!cuFile.exists()) {
+      throw new IOException("Input file not found: " + cuFileName)
+    }
+    val modelString = "-m" + System.getProperty("sun.arch.data.model")
+    val command = "nvcc " + modelString + " -ptx " + cuFile.getPath + " -o " + 
+      ptxFileName
+    println("Executing\n" + command)
+    val process = Runtime.getRuntime.exec(command)
+    val errorMessage = new String(toByteArray(process.getErrorStream))
+    val outputMessage = new String(toByteArray(process.getInputStream))
+    var exitValue = 0
+    try {
+      exitValue = process.waitFor()
+    } catch {
+      case e: InterruptedException => {
+        Thread.currentThread().interrupt()
+        throw new IOException("Interrupted while waiting for nvcc output", e)
+      }
+    }
+    if (exitValue != 0) {
+      println("nvcc process exitValue " + exitValue)
+      println("errorMessage:\n" + errorMessage)
+      println("outputMessage:\n" + outputMessage)
+      throw new IOException("Could not create .ptx file: " + errorMessage)
+    }
+    println("Finished creating PTX file")
+    ptxFileName
+  }
+
+  def get_index(y: Int, x: Int, width: Int): Int = {
+    return x + y * width
+  }
+
+  def nextPowerOfTwo(n: Int): Int = {
+      var i = n - 1
+      i |= (i >>  1)
+      i |= (i >>  2)
+      i |= (i >>  4)
+      i |= (i >>  8)
+      i |= (i >> 16)
+      //i |= (i >> 32)
+      //i - (i >>> 1)
+      return i + 1;
+  }
+
+  // out: [numClusters][numCoords]
+  // objects: [numObjs][numCoords]
+  def cuda_kmeans(objects: Array[Float], numCoords: Int, numObjs: Int, numClusters: Int, threshold: Float, membership: Array[Int], loop_iterations: Array[Int]): Array[Float] = {
+    JCudaDriver.setExceptionsEnabled(true)
+
+    val ptxFileName = preparePtxFile("cuda_kmeans.cu")
+    cuInit(0)
+
+    val device = new CUdevice()
+    cuDeviceGet(device, 0)
+
+    val context = new CUcontext()
+    cuCtxCreate(context, 0, device)
+
+    val module = new CUmodule()
+    cuModuleLoad(module, ptxFileName)
+
+    val function1 = new CUfunction()
+    //cuModuleGetFunction(function1, module, "find_nearest_cluster")
+    cuModuleGetFunction(function1, module, "_Z20find_nearest_clusteriiiPfS_PiS0_")
+
+    val function2 = new CUfunction()
+    //cuModuleGetFunction(function2, module, "compute_delta")
+    cuModuleGetFunction(function2, module, "_Z13compute_deltaPiii")
+
+    var i, j, index, loop = 0 
+    val newClusterSize = Array.fill(numClusters){0} // number of objs assigned in each cluster
+    var delta = 0.0f
+    // [numCoords][numObjs] 
+    val dimObjects = Array.ofDim[Float](numCoords * numObjs)
+    // [numClusters][numCoords] 
+    val clusters= Array.ofDim[Float](numClusters * numCoords)
+    // [numCoords][numClusters]
+    val dimClusters = Array.ofDim[Float](numCoords * numClusters)
+    val newClusters = Array.fill(numCoords * numClusters){0.0f}
+
+    val deviceObjects = new CUdeviceptr()
+    val deviceClusters = new CUdeviceptr()
+    val deviceMembership = new CUdeviceptr()
+    val deviceIntermediates = new CUdeviceptr()
+
+    /* initialize */
+    for (i <- 0 until numCoords) {
+      for (j <- 0 until numObjs) {
+        dimObjects(get_index(i, j, numObjs)) = objects(get_index(j, i, numCoords)) 
+      }
+    }
+    for (i <- 0 until numCoords) {
+      for (j <- 0 until numClusters) {
+        dimClusters(get_index(i, j, numClusters)) = clusters(get_index(j, i, numCoords)) 
+      }
+    }
+    for (i <- 0 until numObjs) {
+      membership(i) = -1
+    }
+    //  To support reduction, numThreadsPerClusterBlock *must* be a power of
+    //  two, and it *must* be no larger than the number of bits that will
+    //  fit into an unsigned char, the type used to keep track of membership
+    //  changes in the kernel.
+    val numThreadsPerClusterBlock = 128
+    val numClusterBlocks = (numObjs + numThreadsPerClusterBlock - 1) / numThreadsPerClusterBlock;
+
+    //#if BLOCK_SHARED_MEM_OPTIMIZATION
+    val clusterBlockSharedDataSize = numThreadsPerClusterBlock * Sizeof.CHAR + numClusters * numCoords * Sizeof.FLOAT
+    
+    val deviceNum = Array(-1)
+    JCuda.cudaGetDevice(deviceNum)
+    val deviceProp = new cudaDeviceProp()
+    JCuda.cudaGetDeviceProperties(deviceProp, deviceNum(0))
+    if(clusterBlockSharedDataSize > deviceProp.sharedMemPerBlock) {
+      println("WARNING: Your CUDA hardware has insufficient block shared memory.")
+      println("You need to recompile with BLOCK_SHARED_MEM_OPTIMIZATION=0. ")
+      println("See the README for details.")
+    }
+
+    //#else
+    //const unsigned int clusterBlockSharedDataSize =numThreadsPerClusterBlock * sizeof(unsigned char);
+    //#endif 
+
+    val numReductionThreads = nextPowerOfTwo(numClusterBlocks)
+    val reductionBlockSharedDataSize = numReductionThreads * Sizeof.INT
+
+    cuMemAlloc(deviceObjects, numCoords * numObjs * Sizeof.FLOAT)
+    cuMemAlloc(deviceClusters, numCoords * numClusters * Sizeof.FLOAT)
+    cuMemAlloc(deviceMembership, numObjs * Sizeof.INT)
+    cuMemAlloc(deviceIntermediates, numReductionThreads * Sizeof.INT) // unsigned data type
+
+    cuMemcpyHtoD(deviceObjects, Pointer.to(dimObjects), numObjs * numCoords * Sizeof.FLOAT)
+    cuMemcpyHtoD(deviceMembership, Pointer.to(membership), numObjs * Sizeof.INT)
+
+    do {
+        cuMemcpyHtoD(deviceClusters, Pointer.to(dimClusters), numClusters * numCoords * Sizeof.FLOAT)
+
+        val kernelParameters = Pointer.to(Pointer.to(Array(numCoords)), Pointer.to(Array(numObjs)), Pointer.to(Array(numClusters)),  Pointer.to(deviceObjects), Pointer.to(deviceClusters), Pointer.to(deviceMembership), Pointer.to(deviceIntermediates))
+
+        // <<< numClusterBlocks, numThreadsPerClusterBlock, clusterBlockSharedDataSize >>>
+        cuLaunchKernel(function1, numClusterBlocks, 1, 1, numThreadsPerClusterBlock, 1, 1, clusterBlockSharedDataSize, null, kernelParameters, null)
+
+        JCuda.cudaDeviceSynchronize()
+        val e = JCuda.cudaGetLastError()
+        printf("CUDA Error %d: %s\n", e, JCuda.cudaGetErrorString(e));
+
+        // (deviceIntermediates, numClusterBlocks, numReductionThreads);
+        val kernelParameters1 = Pointer.to(Pointer.to(deviceIntermediates), Pointer.to(Array(numClusterBlocks)), Pointer.to(Array(numReductionThreads)))
+
+        // compute_delta <<< 1, numReductionThreads, reductionBlockSharedDataSize >>>
+        cuLaunchKernel(function2, 1, 1, 1, numReductionThreads, 1, 1, reductionBlockSharedDataSize, null, kernelParameters1, null) 
+
+        JCuda.cudaDeviceSynchronize()
+
+        val d = Array(0)
+        cuMemcpyDtoH(Pointer.to(d), deviceIntermediates, Sizeof.INT )
+        delta = d(0).toFloat
+        cuMemcpyDtoH(Pointer.to(membership), deviceMembership, numObjs * Sizeof.INT)
+
+        for (i <- 0 until numObjs) {
+          var index = membership(i)
+          newClusterSize(index) += 1;
+          for (j <- 0 until numCoords){
+            //printf("i %d j %d index %d", i, j, index)
+            //printf("newCluster %d idx %d", newClusters.length, get_index(j, index, numClusters))
+            //printf("objects %d idx %d\n", objects.length, get_index(j, index, numObjs))
+            newClusters(get_index(j, index, numClusters)) += objects(get_index(i, j, numCoords))
+          }
+        }
+        
+        //  TODO: Flip the nesting order
+        //  TODO: Change layout of newClusters to [numClusters][numCoords]
+        /* average the sum and replace old cluster centers with newClusters */
+        for (i <- 0 until numClusters) {
+          for (j <- 0 until numCoords) {
+            if (newClusterSize(i) > 0) {
+              dimClusters(get_index(j, i, numClusters)) = newClusters(get_index(j, i, numClusters)) / newClusterSize(i)
+            }
+            newClusters(get_index(j, i, numClusters)) = 0.0f 
+          }
+          newClusterSize(i) = 0
+        }
+
+        delta = delta / numObjs
+        loop += 1
+      } while (delta > threshold && loop < 500)
+    
+    loop_iterations(0) = loop
+
+    for (i <- 0 until numClusters) {
+      for (j <- 0 until numCoords) {
+              clusters(get_index(i, j, numCoords)) = dimClusters(get_index(j, i, numClusters))
+      }
+    }
+
+    cuMemFree(deviceObjects)
+    cuMemFree(deviceClusters)
+    cuMemFree(deviceMembership)
+    cuMemFree(deviceIntermediates)
+    
+    return clusters
+  }
+
+  def write_file (filename: String, numClusters: Int, numObjs: Int, numCoords: Int, clusters: Array[Float], membership: Array[Int]): Int = {
+
+    var outFileName = filename + ".cluster_centres"
+    printf("Writing coordinates of K=%d cluster centers to file \"%s\"\n", numClusters, outFileName)
+ 
+    for(i <- 0 until numClusters){
+      printf("%d ", i)
+      for(j <- 0 until numCoords){
+        printf("%f ", clusters(get_index(i, j, numCoords)))
+      }
+      printf("\n")
+    }
+
+    outFileName = filename + ".membership"
+    printf("Writing membership of N=%d data objects to file \"%s\"\n", numObjs, outFileName)
+    for(i <- 0 until numObjs){
+      printf("%d %d\n", i, membership(i))
+    }
+    return 0 
+  }
+
+  def read_file (filename: String, numObjs: Array[Int], numCoords: Array[Int]): Array[Float] = {
+
+    // [numClusters][numCoords] 
+    val source = scala.io.Source.fromFile(filename)
+    //val source = scala.io.Source.fromFile("/Users/qijing.huang/Documents/CS267Project/kmeans/Image_data/color100.txt")
+    val lines = (try source.mkString finally source.close()).split('\n').map(x => x.trim)
+    numObjs(0) = lines.length
+    val cols = lines(0).split(' ')
+
+    numCoords(0) = cols.length - 1
+
+    val objects = lines.flatMap(x => x.split(' ').slice(1, x.length).map(x => x.trim.toFloat))
+    return objects
+  }
+
+  private def toByteArray(inputStream: InputStream): Array[Byte] = {
+    val baos = new ByteArrayOutputStream()
+    val buffer = Array.ofDim[Byte](8192)
+    while (true) {
+      val read = inputStream.read(buffer)
+      if (read == -1) {
+        //break
+      }
+      baos.write(buffer, 0, read)
+    }
+    baos.toByteArray()
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
