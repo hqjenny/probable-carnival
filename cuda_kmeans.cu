5c709f86b7961970593c6309f562101e9eca7314
James Jia <jamesjia@berkeley.edu>
2016-04-24 12:52:59 -0700

    Moved format tweets script and started working on predict

--- cuda_kmeans.cu
+++ cuda_kmeans.cu
@@ -191,6 +191,66 @@ void compute_delta(int *deviceIntermediates,
     }
 }
 
+
+__global__ static
+void predict(int numCoords,
+                          int numObjs,
+                          int numClusters,
+                          float *objects,           //  [numCoords][numObjs]
+                          float *deviceClusters,    //  [numCoords][numClusters]
+                          int *membership,          //  [numObjs]
+                          int *intermediates)
+{
+    //  The type chosen for membershipChanged must be large enough to support
+    //  reductions! There are blockDim.x elements, one for each thread in the
+    //  block. See numThreadsPerClusterBlock in cuda_kmeans().
+#if BLOCK_SHARED_MEM_OPTIMIZATION
+    float *clusters = (float *)(sharedMemory + blockDim.x);
+#else
+    float *clusters = deviceClusters;
+#endif
+
+#if BLOCK_SHARED_MEM_OPTIMIZATION
+    //  BEWARE: We can overrun our shared memory here if there are too many
+    //  clusters or too many coordinates! For reference, a Tesla C1060 has 16
+    //  KiB of shared memory per block, and a GeForce GTX 480 has 48 KiB of
+    //  shared memory per block.
+    for (int i = threadIdx.x; i < numClusters; i += blockDim.x) {
+        for (int j = 0; j < numCoords; j++) {
+            clusters[numClusters * j + i] = deviceClusters[numClusters * j + i];
+        }
+    }
+    __syncthreads();
+#endif
+
+    int objectId = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (objectId < numObjs) {
+        int   index, i;
+        float dist, min_dist;
+
+        /* find the cluster id that has min distance to object */
+        index    = 0;
+        min_dist = euclid_dist_2(numCoords, numObjs, numClusters,
+                                 objects, clusters, objectId, 0);
+
+        for (i=1; i<numClusters; i++) {
+            dist = euclid_dist_2(numCoords, numObjs, numClusters,
+                                 objects, clusters, objectId, i);
+            /* no need square root */
+            if (dist < min_dist) { /* find the min and its array index */
+                min_dist = dist;
+                index    = i;
+            }
+        }
+
+        /* assign the membership to object objectId */
+        membership[objectId] = index;
+
+        __syncthreads();    //  For membershipChanged[]
+    }
+}
+
 /*----< cuda_kmeans() >-------------------------------------------------------*/
 //
 //  ----------------------------------------
