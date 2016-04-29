8bf77e5753c9588ece6aabcc8f373c11847760d4
James Jia <jamesjia@berkeley.edu>
2016-04-29 14:50:26 -0700

    added predict to script

--- feelthebern.sh
+++ feelthebern.sh
@@ -4,4 +4,5 @@
 
 #~/spark/bin/spark-submit --driver-memory 10g --class Kmeans --jars $(find /root/JCuda-All-0.7.5-bin-Linux-x86_64/| grep '\.jar'|tr '\n' ',')  target/scala-2.10/kmeans_2.10-1.0.jar train /root/input-tweets.txt /root/probable-carnival/out 7 0.0001 1000
 ~/spark/bin/spark-submit --class Kmeans --jars $(find /root/JCuda-All-0.7.5-bin-Linux-x86_64/| grep '\.jar'|tr '\n' ',')  target/scala-2.10/kmeans_2.10-1.0.jar train /root/kmeans/Image_data/color100.txt /root/probable-carnival/out 7 0.0001 1000
+# ~/spark/bin/spark-submit --class Kmeans --master spark://ec2-54-186-103-10.us-west-2.compute.amazonaws.com:7077 --jars $(find /root/JCuda-All-0.7.5-bin-Linux-x86_64/| grep '\.jar'|tr '\n' ',')  target/scala-2.10/kmeans_2.10-1.0.jar predict /root/input-tweets.txt /root/probable-carnival/out.cluster_centres /root/probable-carnival/predicted_out
 
