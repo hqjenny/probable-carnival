3c9a9cafdd915ac10f9abf6ce6eff9f939ab7494
root <root@ip-172-31-15-120.us-west-2.compute.internal>
2016-05-02 00:13:51 +0000

    fixed scala script

---
+++ scripts/build.sbt
@@ -0,0 +1,12 @@
+name := "format_tweets"
+
+version := "1.0"
+
+scalaVersion := "2.10.4"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.4.0" % "provided"
+
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "1.4.0" % "provided"
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "1.4.0" % "provided"
+
--- scripts/format_tweets_for_kmeans.scala
+++ scripts/format_tweets_for_kmeans.scala
@@ -1,13 +1,20 @@
-#!/bin/sh
-exec scala "$0" "$@"
-!#
-
 object FormatTweetsForCUDA {
     import org.apache.spark.mllib.linalg.Vector
     import org.apache.spark.mllib.feature.HashingTF
     import java.io._
-
+    import org.apache.spark.SparkContext
+    import org.apache.spark.SparkContext._
+    import org.apache.spark.SparkConf
+    import org.apache.spark.rdd._
+    import org.apache.spark.sql.SQLContext
+    import org.apache.spark.sql._
+    import org.apache.spark.sql.SQLContext._
     def main(args: Array[String]) {
+        val conf = new SparkConf().setAppName("FormatTweetsForCUDA")
+        val sc = new SparkContext(conf)
+        val sqlContext= new SQLContext(sc)
+
+        import sqlContext.implicits._ 
         if (args.length < 2) {
             System.err.println("Usage: " + this.getClass.getSimpleName +
                 "<input_file> <output_file>")
@@ -22,7 +29,7 @@ object FormatTweetsForCUDA {
         val tweetTable = sqlContext.read.json(tweetInput).cache()
         tweetTable.registerTempTable("tweetTable")
         val texts = sqlContext.sql("SELECT text from tweetTable").map(_.toString)
-        val vectorsRDD = texts.map(featurize).zipWithIndex.map(writeFormat)
+        val vectorsRDD = texts.map(featurize(tf)).zipWithIndex.map(writeFormat)
         val vectors = vectorsRDD.collect()
 
         val file = args(1)
@@ -33,7 +40,7 @@ object FormatTweetsForCUDA {
         writer.close()
     }
 
-    def featurize(s: String): Vector = {
+    def featurize(tf: HashingTF)( s: String): Vector = {
         tf.transform(s.sliding(2).toSeq)
     }
 
@@ -41,5 +48,3 @@ object FormatTweetsForCUDA {
         t._2 + " " + (t._1.toArray mkString " ")
     }
 }
-
-FormatTweetsForCUDA.main(args)
