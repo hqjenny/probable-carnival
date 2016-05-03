9d33480988528213565199f9153d930d4e373fe0
James Jia <jamesjia@berkeley.edu>
2016-05-02 21:02:23 -0700

    dblock shared mem opt off

--- feelthebern.sh
+++ feelthebern.sh
@@ -1,5 +1,5 @@
 #./sbt/sbt package
-#nvcc -m64 -ptx cuda_kmeans.cu -o cuda_kmeans.ptx
+# nvcc -m64 -ptx cuda_kmeans.cu -o cuda_kmeans.ptx -DBLOCK_SHARED_MEM_OPTIMIZATION=0
 #~/spark/bin/spark-submit --class Kmeans --master spark://ec2-54-186-103-10.us-west-2.compute.amazonaws.com:7077 --jars $(find /root/JCuda-All-0.7.5-bin-Linux-x86_64/| grep '\.jar'|tr '\n' ',')  target/scala-2.10/kmeans_2.10-1.0.jar train /root/input-tweets.txt /root/probable-carnival/out 10 0.0001 20
 
 #~/spark/bin/spark-submit --driver-memory 10g --class Kmeans --jars $(find /root/JCuda-All-0.7.5-bin-Linux-x86_64/| grep '\.jar'|tr '\n' ',')  target/scala-2.10/kmeans_2.10-1.0.jar train /root/input-tweets.txt /root/probable-carnival/out 7 0.0001 1000
